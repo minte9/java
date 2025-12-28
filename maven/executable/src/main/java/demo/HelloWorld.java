27cb48f5ad63f94487c341d05e078262b9f087bb
minte9 <catalinprescure@gmail.com>
2025-12-28 16:04:03 +0200

    java-pages update

---
+++ maven/executable/src/main/java/demo/HelloWorld.java
@@ -0,0 +1,30 @@
+/**
+ * MAVEN BUILD - EXECUTABLE (FAT JAR)
+ * ----------------------------------
+ * This example demonstrates how to build a FAT jar using Maven.
+ * 
+ * A fat JAR:
+ * ----------
+ *  - Contains application classes
+ *  - Contains ALL external dependencies
+ *  - can be executed directly wiht java -jar 
+ * 
+ * EXTERNAL LIBERY USED:
+ * ---------------------
+ * Apache Common Lang (StringUtils)
+ */
+
+package demo;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        String text = "hello maven";
+        String result = StringUtils.capitalize(text);
+
+        System.out.println(result);
+            // Hello maven
+    }
+}
