b1aec5c13d8b2eba970f538845dccce1229113b2
minte9 <catalinprescure@gmail.com>
2025-12-29 20:24:24 +0200

    java-pages update

---
+++ java-se/pages/src/main/java/com/minte9/streams/common_operations/StreamReduce.java
@@ -0,0 +1,42 @@
+/**
+ * STREAM - REDUCE
+ * ------------------------------------------------------------
+ * Wwhen you want a single result from collection.
+ * For example, to calculate the sum.
+ * 
+ * Imperative approach - using for loop
+ * Declarative approach - acc is the "accumulator" (current sum)
+ */
+
+package com.minte9.streams.common_operations;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamReduce {
+    public static void main(String[] args) {
+
+        List<Integer> source = Arrays.asList(1, 2, 3);
+        int expected = 6;
+
+        assertEquals(expected, reduceToSumLoop(source));
+        assertEquals(expected, reduceToSumStream(source));
+        
+        System.out.println("Done");
+    }
+
+    public static int reduceToSumLoop(List<Integer> lst) {
+        int sum = 0;
+        for (Integer i : lst) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int reduceToSumStream(List<Integer> lst) {
+        return lst.stream()
+                  .reduce(0, (acc, x) -> acc + x);
+    }
+}
