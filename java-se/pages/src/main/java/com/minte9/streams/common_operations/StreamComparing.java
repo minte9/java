1d8031eafaae7c7cf72faa1c5edb0d3b5b8f5653
minte9 <catalinprescure@gmail.com>
2025-12-29 20:32:43 +0200

    java-pages update

---
+++ java-se/pages/src/main/java/com/minte9/streams/common_operations/StreamComparing.java
@@ -0,0 +1,37 @@
+/**
+ * STREAM - COMPARING
+ * ------------------------------------------------------------
+ * Comparing is used when you want to find minimum or maximum 
+ * of an element.
+ */
+
+package com.minte9.streams.common_operations;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class StreamComparing {
+    public static void main(String[] args) {
+
+        List<Student> students = Arrays.asList(
+            new Student("John", 23),
+            new Student("Mary", 30),
+            new Student("Mike", 27)
+        );
+
+        Student expected = students.get(0);
+        Student youngest = students
+                    .stream()
+                    .min(Comparator.comparing(x -> x.age()))
+                    .get();
+
+        assertEquals(expected, youngest); // pass
+
+        System.out.println("Done");
+    }
+}
+
+record Student(String name, int age) {}
