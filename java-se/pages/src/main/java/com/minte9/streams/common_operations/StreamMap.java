3e8d972dc9aef504539f20aa71600eb166d356ef
minte9 <catalinprescure@gmail.com>
2025-12-29 20:02:50 +0200

    java-pages update

---
+++ java-se/pages/src/main/java/com/minte9/streams/common_operations/StreamMap.java
@@ -0,0 +1,44 @@
+/**
+ * STREAMS - MAP
+ * --------------
+ * Map each value to uppercase.
+ */
+
+package com.minte9.streams.common_operations;
+
+import java.util.ArrayList;
+import java.util.List;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertEquals;
+
+public class StreamMap {
+    public static void main(String[] args) {
+
+        List<String> source, expected;
+
+        source = asList("a", "b", "c");
+        expected = asList("A", "B", "C");
+        
+        assertEquals(expected, toUpper(source));        // pass
+        assertEquals(expected, toUpperStream(source));  // pass
+
+        System.out.println("Done");
+    }
+
+    private static List<String> toUpper(List<String> lst) {
+        List<String> result = new ArrayList<>();
+
+        for (String s: lst) {
+            result.add(s.toUpperCase());
+        }
+        return result;
+    }
+
+    private static List<String> toUpperStream(List<String> lst) {
+        return lst.stream()
+                  .map(x -> x.toUpperCase())  // lambda OR
+                  .map(String::toUpperCase)   // method reference
+                  .collect(toList());
+    }
+}
