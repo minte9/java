38fb3b90c3cf130ae4bdee6c5fd2ca606695f042
minte9 <catalinprescure@gmail.com>
2025-12-29 20:40:06 +0200

    java-pages update

--- java-se/pages/src/main/java/com/minte9/streams/common_operations/StreamPartitioningBy.java
+++ java-se/pages/src/main/java/com/minte9/streams/common_operations/StreamPartitioningBy.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class PartitioningBy {
+public class StreamPartitioningBy {
     public static void main(String[] args) {
 
       List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
