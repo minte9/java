69b7120afc0d0b65ecf4b657f7815a0eeb866386
minte9 <catalinprescure@gmail.com>
2025-12-30 15:45:22 +0200

    java-pages update

---
+++ java-se/pages/src/main/java/com/minte9/threads/executor_service/PeriodicTaskForever.java
@@ -0,0 +1,35 @@
+/**
+ * PERIODIC TASK - RUN FOREVER
+ * ---------------------------
+ * This simulates a background service.
+*/
+package com.minte9.threads.executor_service;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class PeriodicTaskForever {
+    public static void main(String[] args) {
+        
+        ScheduledExecutorService scheduler = 
+            Executors.newSingleThreadScheduledExecutor();
+        
+        scheduler.scheduleAtFixedRate(
+            new Worker(), 
+            0,  // initial delay
+            5,  // period
+            TimeUnit.SECONDS
+        );
+    }
+}
+
+class Worker implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(
+            "Heartbeat from " +
+            Thread.currentThread().getName()
+        );
+    }
+}
