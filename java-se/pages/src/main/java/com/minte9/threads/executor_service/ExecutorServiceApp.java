6b01e84963e253f21f9ba53e389db5bb23f23b8c
minte9 <catalinprescure@gmail.com>
2026-01-01 11:48:25 +0200

    java-pages update

--- java-se/pages/src/main/java/com/minte9/threads/executor_service/ExecutorServiceApp.java
+++ java-se/pages/src/main/java/com/minte9/threads/executor_service/ExecutorServiceApp.java
@@ -14,7 +14,7 @@
  *  - Limits concurrency
  *  - Resuses threads efficientlty
  * 
- * TAKEAWAY: ⚠️ 🟡
+ * TAKEAWAY: ⚠️
  * ---------
  * An ExecutorSerice manages threads for you.
  * You submit tasks, not threads.
