3b9eace1a7ebe9d84ef10b0cf66f030ad897cda8
minte9 <catalinprescure@gmail.com>
2026-01-01 21:43:07 +0200

    java-pages update

--- spring-boot/quick_start/src/main/java/demo/App.java
+++ spring-boot/quick_start/src/main/java/demo/App.java
@@ -6,7 +6,7 @@
  * Add dependency: Spring Web
  */
 
-package com.minte9.quick_start;
+package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
