27f144ae452b482c6b3b2856b48cb0d8a27d2024
minte9 <catalinprescure@gmail.com>
2026-03-23 14:59:12 +0200

    java-pages update

--- java-se/pages/src/main/java/com/minte9/streams/streams_basics/IterationStyles.java
+++ java-se/pages/src/main/java/com/minte9/streams/streams_basics/IterationStyles.java
@@ -6,21 +6,21 @@
  * 
  * TWO APPROACHES:
  * ---------------
- * 1) External iteration (imperative)
- *      - The developer controls HOW iteration happens 
- *      - Uses loops or iterators
- *      - Requires mutable state (counter, variables)
+ * External iteration (imperative):
+ *  - The developer controls HOW iteration happens 
+ *  - Uses loops or iterators
+ *  - Requires mutable state (counter, variables)
  * 
- * 2) Internal iteration (declarative)
- *      - The library controls HOW iteration hapens
- *      - The developer specifies WHAT should be done
- *      - No explicit loops or mutable counters
+ * Internal iteration (declarative):
+ *  - The library controls HOW iteration hapens
+ *  - The developer specifies WHAT should be done
+ *  - No explicit loops or mutable counters
  * 
  * Iterator - external iterator
  * Stream   - internal iterator
  */
 
-package com.minte9.streams.iteration_styles;
+package com.minte9.streams.streams_basics;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -30,12 +30,10 @@ public class IterationStyles {
     public static void main(String[] args) {
         
         List<Integer> numbers = Arrays.asList(1, 2, 3);
+        int count;
 
-        // -------------------------------------
-        // 1) External iteration - for-each loop
-        // -------------------------------------
-        int count = 0;
-
+        // External iteration (for-each loop)
+        count = 0;
         for (int n : numbers) {
             if (n <= 2) {
                 count++;
@@ -43,11 +41,8 @@ public class IterationStyles {
         }
         System.out.println(count);  // 2
 
-        // -------------------------------------
-        // 2) External iteration - Iterator
-        // -------------------------------------
+        // External iteration (Iterator)
         count = 0;
-
         Iterator<Integer> it = numbers.iterator();
         while(it.hasNext()) {
             int n = it.next();
@@ -57,12 +52,11 @@ public class IterationStyles {
         }
         System.out.println(count);  // 2
 
-        // -------------------------------------
-        // 3) Internal iteration - Stream
-        // -------------------------------------
-        long total = numbers.stream()
-                            .filter(n -> n <= 2)
-                            .count();
+        // Internal iteration - Stream
+        long total = 
+            numbers.stream()
+                   .filter(n -> n <= 2)
+                   .count();
         System.out.println(total);  // 2
     }    
 }
